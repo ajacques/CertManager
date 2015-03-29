d5be4d9fcc46456ff8b420f8236fcbdefeda9005
Adam Jacques <adam@technowizardry.net>
2015-03-28 23:54:51 -0700

    Fixed user creation logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,40 @@
-require 'securerandom'
-
 class UsersController < ApplicationController
+  helper_method :validates?
   def index
     @users = User.all
   end
 
   def new
+    @validations = flash[:validations]
+    data = flash.try(:[], :data)
+    @user = if data
+      User.create data
+    else
+      User.new
+    end
   end
 
   def create
     user = User.create user_params
-    user.password = SecureRandom.urlsafe_base64
+    user.randomize_password
     user.create_confirm_token
-    user.save!
-    UserMailer.new_account(user).deliver_later
-    redirect_to user
+    if user.invalid?
+      logger.info "Failed to save record #{user_params}. Validations failed #{user.errors.inspect}"
+      flash[:validations] = user.errors
+      flash[:data] = user
+      redirect_to action: :new
+      return
+    end
+    begin
+      user.save!
+    rescue ActiveRecord::RecordInvalid => err
+      logger.info "Failed to save record #{user_params} #{err}}"
+      flash[:error] = err.to_s
+      redirect_to action: :new
+    else
+      UserMailer.new_account(user).deliver_now
+      redirect_to user
+    end
   end
 
   def show
@@ -24,5 +44,12 @@ class UsersController < ApplicationController
 
   def user_params
     params[:user].permit(:first_name, :last_name, :email)
+  end
+
+  protected
+  def validates?(selector, if_true)
+    if_true if @validations
+      .try(:[], selector.to_s)
+      .try(:any?)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,33 +5,34 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :registerable, :recoverable, :rememberable, :trackable, :validatable
   attr_accessor :password
-  validates :email, :password, presence: true
-  validates :email, uniqueness: true
+  validates :password, :first_name, :last_name, presence: true
+  validates :email, email: true, uniqueness: true
   validates :password, length: { within: 6..128 }
-  validates :password, confirmation: true
   before_save :update_password
 
   def password_matches?(pwd)
     password_hash == User.hash_password(pwd, password_salt)
   end
   def create_confirm_token
-    confirmation_token = SecureRandom.urlsafe_base64(32)
-    confirmation_sent_at = Time.now
+    self.confirmation_token = SecureRandom.urlsafe_base64(32)
+    self.confirmation_sent_at = Time.now
+  end
+  def randomize_password
+    self.password = SecureRandom.urlsafe_base64(32)
   end
   def to_s
     "#{first_name} #{last_name}"
   end
   def self.authenticate(username, password)
     user = find_by_email(username)
-    return nil  if user.nil?
-    return user if user.password_matches?(password) and user.can_login?
+    return user.try(:password_matches?, password) and user.try(:can_login?)
   end
 
   private
   def update_password
     if self.password
       self.password_salt = SecureRandom.random_bytes(32)
-      self.password_hash = User.hash_password(password, self.password_salt)
+      self.password_hash = User.hash_password(password, password_salt)
     end
   end
   def self.hash_password(pwd, salt)
