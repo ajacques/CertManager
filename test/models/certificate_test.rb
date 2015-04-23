d2d6173a1b41cba6a1d6b399026275e3a597e06c
Adam Jacques <adam@technowizardry.net>
2015-04-22 22:43:28 -0700

    Fixing unit test

--- test/models/certificate_test.rb
+++ test/models/certificate_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CertificateTest < ActiveSupport::TestCase
   test 'can self sign' do
     ca = certificates(:rsa_ca)
-    ca.sign(ca.public_key)
+    ca.sign(ca)
     assert ca.signed?
     assert ca.can_sign?
   end
