a40a3654a7e01c8f83a4bc35a027d9d08eb101b3
Adam Jacques <adam@technowizardry.net>
2015-05-27 00:08:39 -0700

    Resolved a few issues related to Docker containers. Temporarily removed devise traits from User model. Revisit this later

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ require 'securerandom'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :registerable, :recoverable, :rememberable, :trackable, :validatable
   attr_accessor :password, :password_confirmation, :confirmation_token_confirmation
   validates :first_name, :last_name, presence: true
   validates :email, email: true, uniqueness: true
---
+++ unicorn.rb
@@ -0,0 +1,11 @@
+listen 3000
+preload_app true
+worker_processes 4
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
