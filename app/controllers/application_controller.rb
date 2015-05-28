1d310baa36e9f52ef9135cf440961863ccffa148
Adam Jacques <adam@technowizardry.net>
2015-05-28 00:06:12 -0700

    Place dev server PID in /tmp/ to avoid cross docker container conflicts. Fixed an issue caused by the user requesting GET /login while already logged in Moved Dockerfiles to new folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :require_login
+  append_before_action :initialize_user
   helper_method :user_signed_in?
   helper_method :current_user
 
@@ -10,13 +11,15 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user
   end
+  def initialize_user
+    @current_user = User.find session[:user_id] if session.has_key? :user_id
+  end
   def require_login
     unless user_signed_in?
       session[:return_url] = request.fullpath
       redirect_to new_user_session_path
       return
     end
-    @current_user = User.find session[:user_id]
   end
   def user_signed_in?
     session.has_key?(:user_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 require 'securerandom'
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   attr_accessor :password, :password_confirmation, :confirmation_token_confirmation
   validates :first_name, :last_name, presence: true
   validates :email, email: true, uniqueness: true
