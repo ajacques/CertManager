bac37ab70497e1b6075e2ba41f6539a32b44666e
Adam Jacques <adam@technowizardry.net>
2015-08-20 18:35:33 -0700

    Fixed CertBundle

--- app/assets/javascripts/models/cert_bundle.js
+++ app/assets/javascripts/models/cert_bundle.js
@@ -1,14 +1,40 @@
 var CertBundle = function(data) {
   'use strict';
   var regex = /-----BEGIN ([A-Z ]+)-----[\n\r]{1,2}([a-zA-Z0-9=/+\r\n]+)-----END ([A-Z ]+)-----/g;
-  var certs = data.match(regex);
+  var matches = data.match(regex);
+  var certs = [];
+  var unknown = [];
+  var keys = [];
 
-  this.chunks = certs.map(function(cert) {
+  var chunks = matches.map(function(cert) {
     regex.lastIndex = 0;
     return regex.exec(cert);
   });
 
-  for (var chunk : this.chunks) {
+  var chunk_valid = function(chunk) {
+    return chunk[1] == chunk[3];
+  };
 
+  for (var i = 0; i < chunks.length; i++) {
+    var chunk = chunks[i];
+    if (chunk_valid(chunk)) {
+      if (chunk[1] === "CERTIFICATE") {
+        certs.push(chunk[0]);
+      } else if (chunk[1] === "RSA PRIVATE KEY") {
+        keys.push(chunk[0])
+      } else {
+        unknown.push(chunk[0]);
+      }
+    }
   }
+
+  this.get_certs = function() {
+    return certs;
+  };
+  this.get_keys = function() {
+    return keys;
+  };
+  this.get_unknown = function() {
+    return unknown;
+  };
 };
