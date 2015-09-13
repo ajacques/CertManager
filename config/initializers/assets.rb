5e64224fc61f2371dba7a3c7bc1c0f4b8a904540
Adam Jacques <adam@technowizardry.net>
2015-09-13 16:42:53 -0700

    Fix HAML asset generation
    URL helpers were not being mounted correctly so all
    hyperlinks inside the HAML asset context were incorrect

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,17 +1,19 @@
 # Be sure to restart your server when you modify this file.
+Rails.application.configure do
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+  # Precompile additional assets.
+  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  # config.assets.precompile += %w( search.js )
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+  assets.register_mime_type 'text/html', extensions: ['.html']
+  assets.register_engine '.haml', Tilt::HamlTemplate
+  config.assets.paths << Rails.root.join('app', 'assets', 'templates')
 
-Rails.application.assets.register_mime_type 'text/html', extensions: ['.html']
-Rails.application.assets.register_engine '.haml', Tilt::HamlTemplate
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'templates')
-
-Rails.application.assets.context_class.class_eval do
-  include ActionView
-  include ActionView::Helpers
+  assets.context_class.class_eval do
+    include Rails.application.routes.url_helpers
+    include Rails.application.routes.mounted_helpers
+    include ActionView::Helpers
+  end
 end
