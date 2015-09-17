cc03c90dab81cb74025cc3fe1cdf81d75c2f0b93
Adam Jacques <adam@technowizardry.net>
2015-09-16 20:22:11 -0700

    Add HTML certificate expiration email template
    This format will contain more detailed information
    and actions than the text only version.

--- app/mailers/certificate_mailer.rb
+++ app/mailers/certificate_mailer.rb
@@ -1,11 +1,15 @@
 class CertificateMailer < BaseMailer
   def expiration_notice(user, cert_list)
     @certificates = cert_list
+    @user = user
     headers 'Importance' => 'High',
       'X-Notify-Type' => 'Certificate-Expiration',
       'X-Priority' => '1'
 
     mail to: user.email_addr, subject: 'Certificate Expiration Notice' do |format|
+      format.html {
+        render layout: 'mailers/single_column'
+      }
       format.text
     end
   end
