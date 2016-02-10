08e4cff44222137e2011ea5a0eb1265b3e0e4c78
Adam Jacques <adam@technowizardry.net>
2016-02-10 21:02:37 +0000

    Sort of apply SMTP settings from DB

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,7 +1,9 @@
 class SettingsController < ApplicationController
   def update
     set = SettingSet.new
-    set.assign_attributes params.require(:setting_set).permit(:acme_client)
+    set.assign_attributes params.require(:setting_set).permit(:acme_client, :smtp_address, :smtp_port)
+    ActionMailer::Base.smtp_settings['address'] = set.smtp_address if set.smtp_address_changed?
+    ActionMailer::Base.smtp_settings['port'] = set.smtp_port if set.smtp_port_changed?
     set.save!
 
     redirect_to settings_path
