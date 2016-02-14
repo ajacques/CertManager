cf3aec653709680bac8412386a7d522481974a07
Adam Jacques <adam@technowizardry.net>
2016-02-14 06:04:42 +0000

    Fix certificate creation
    Disable SANs on CSRs

--- app/assets/javascripts/controls/react/subject_alt_name_list.js.jsx
+++ app/assets/javascripts/controls/react/subject_alt_name_list.js.jsx
@@ -1,8 +1,23 @@
 (function(root) {
   'use strict';
   root.SubjectAltNameList = React.createClass({
+    getInitialState: function() {
+      return {names: ''};
+    },
+    handleChange: function(event) {
+      this.setState({names: event.target.value});
+    },
+    _inputBox: function(text) {
+      var name = undefined;
+      if (text !== '') {
+        name = "certificate[csr_attributes][subject_alternate_names][]";
+      }
+      return (
+        <input name={name} onChange={this.handleChange} type="text" className="san-textbox form-control" />
+      );
+    },
     render: function() {
-      return (<input name="certificate[csr_attributes][subject_alternate_names][]" type="text" className="san-textbox form-control" />);
+      return this._inputBox(this.state.names);
     }
   });
 })(this);
