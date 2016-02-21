7ae5a98ef7492061b265b3135a61427aa86d7f77
Adam Jacques <adam@technowizardry.net>
2016-02-21 02:07:37 +0000

    Update user info on login

--- app/controllers/concerns/authenticator.rb
+++ app/controllers/concerns/authenticator.rb
@@ -4,6 +4,10 @@ module Authenticator
   protected
 
   def assume_user(user)
+    user.sign_in_count += 1
+    user.last_sign_in_at = Time.now
+    user.save!
+
     reset_session
     session[:user_id] = user.id
   end
