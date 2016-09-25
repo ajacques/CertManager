a1a6ab5d79e9ae5a100f1355e73d68728bc2e23a
Adam Jacques <adam@technowizardry.net>
2016-09-25 13:09:42 -0700

    Update static files for Rails 5

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,8 +15,8 @@ Rails.application.configure do
   config.active_support.test_order = :random
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_files = true
-  config.static_cache_control = 'public, max-age=3600'
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
