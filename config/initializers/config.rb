8c7550a785ecb506cd85be0e173083ba2dbdc721
Adam Jacques <adam@technowizardry.net>
2016-09-28 23:43:50 -0700

    Remove old mail config code
    Replaced with DB config storage

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,16 +1,6 @@
 require 'app/configuration'
 require 'app/instrumented_redis'
 
-email_delivery = CertManager::Configuration.email_delivery
-secret_key = CertManager::Configuration.secret_key
 resque = CertManager::Configuration.resque
-
-if email_delivery
-  email_delivery.each do |key, value|
-    ActionMailer::Base.send("#{key}=", value)
-  end
-end
-
-Rails.application.config.secret_token = secret_key if secret_key
 
 Resque.redis = resque if resque
