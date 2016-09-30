f9dcb0f1a20ab3e12c55fd01a0bc78be9a6adf5b
Adam Jacques <adam@technowizardry.net>
2016-09-29 23:06:20 -0700

    Enable STARTLS control for SMTP

--- app/models/settings/email_server.rb
+++ app/models/settings/email_server.rb
@@ -1,5 +1,5 @@
 class Settings::EmailServer < Settings::Group
-  config_keys :server, :port, :from_address
+  config_keys :server, :port, :from_address, :enable_starttls
 
   def delivery_method
     'smtp'
@@ -8,8 +8,8 @@ class Settings::EmailServer < Settings::Group
   def action_mailer_settings
     {
       address: server,
-      domain: '',
-      port: port
+      port: port,
+      enable_starttls_auto: enable_starttls
     }
   end
 end
