5f4e6cdc93d43906f26581e535d65d1cae312611
Adam Jacques <adam@technowizardry.net>
2016-10-01 10:07:17 -0700

    Can now mount non-div based React components

--- app/assets/javascripts/critical/AppInvoker.js
+++ app/assets/javascripts/critical/AppInvoker.js
@@ -1,11 +1,9 @@
 (function() {
-  var page_inst;
-
   window.on_pageload = window.on_pageload || [];
 
   function initializeReact() {
     var className, clazz, element, j, len, props, ref, results;
-    ref = document.querySelectorAll('div[data-react-mount=true]');
+    ref = document.querySelectorAll('[data-react-mount=true]');
     results = [];
     for (j = 0, len = ref.length; j < len; j++) {
       element = ref[j];
@@ -18,29 +16,28 @@
   }
 
   const PageInitializer = function() {
-    var d, i, j, len, page_name, page_obj, ref;
+    var i, j, len, page_obj, ref;
     ref = window.on_pageload;
     for (i = j = 0, len = ref.length; j < len; i = ++j) {
-      d = ref[i];
-      d();
+      ref[i]();
     }
     window.on_pageload = {
       push: function(item) {
-        return item();
+        item();
       }
     };
-    page_name = document.body.getAttribute('data-page');
+    var page_name = document.body.getAttribute('data-page');
     page_obj = window[page_name];
     if ("function" === typeof page_obj) {
       page_obj();
     } else if ("object" === typeof page_obj) {
       page_obj.init();
     }
-    return initializeReact();
+    initializeReact();
   };
 
   if (document.readyState === "complete" || document.readyState === "loaded") {
-    page_inst = PageInitializer();
+    PageInitializer();
   } else {
     document.addEventListener('DOMContentLoaded', PageInitializer);
   }
