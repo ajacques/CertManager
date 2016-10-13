39e901097b954cd527a27b28e868ee75a9922bc5
Adam Jacques <adam@technowizardry.net>
2016-10-12 17:41:33 -0700

    Rubocop

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,5 +1,5 @@
 module LinkHelper
-  def untrusted_party_link(name, url, opts={}, &block)
+  def untrusted_party_link(name, url, opts = {}, &block)
     link_to name, url, opts.merge(target: '_blank', rel: 'noopener', &block)
   end
 end
