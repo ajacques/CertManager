993f292980a7a131c72c458bf219454491e938cc
Adam Jacques <adam@technowizardry.net>
2016-12-20 14:03:07 -0800

    Improved certificates#new

--- app/assets/javascripts/controls/react/pages/certificates/new/SubjectAltNameList.es6.jsx
+++ app/assets/javascripts/controls/react/pages/certificates/new/SubjectAltNameList.es6.jsx
@@ -1,30 +1,34 @@
+/* exported SubjectAltNameList */
 class SubjectAltNameList extends React.Component {
   constructor(props) {
     super(props);
+    this.addRow = this.addRow.bind(this);
     this.renderRow = this.renderRow.bind(this);
     this.state = {
       names: []
     };
   }
-  _textBox() {
+  addRow(event) {
+    event.preventDefault();
+    const newList = this.state.names.slice();
+    newList.push('');
+    this.setState({names: newList});
+  }
+  renderRow() {
     return (
-      <input name="certificate[csr_attributes][subject_alternate_names][]" 
+      <input name="certificate[csr_attributes][subject_alternate_names][]"
         className="san-textbox form-control" type="text" onChange={this.handleChange} />
     );
   }
-  renderRow(item, index) {
-    return (
-      <div>
-        {this._textBox()}
-      </div>
-    );
+  buttonBar() {
+    return <button className="btn btn-inline" onClick={this.addRow}>+</button>;
   }
   render() {
     return (
-      <ul>
+      <div>
         {this.state.names.map(this.renderRow)}
-        {this._textBox()}
-      </ul>
+        {this.buttonBar()}
+      </div>
     );
   }
 }
