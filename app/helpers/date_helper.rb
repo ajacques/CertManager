6b8e71a4092859db73cd706d415ccebd63800bfe
Adam Jacques <adam@technowizardry.net>
2017-01-01 18:04:42 -0800

    Fix for null timestamps

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,5 +1,6 @@
 module DateHelper
   def inline_ago_string(timestamp, format: :long)
+    return unless timestamp
     tz_adjusted = localize timestamp.in_time_zone(current_user.time_zone), format: format
     ago = time_ago_in_words(timestamp)
     translate 'time.composed.inline_string', time: tz_adjusted, ago: ago
