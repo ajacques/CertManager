fec7dc95fe68e82373689dd1b6ea1723b4bbe14e
Adam Jacques <adam@technowizardry.net>
2017-02-22 18:44:04 -0800

    Fix Docker build

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,7 +1,7 @@
 require 'app/configuration'
 require 'app/instrumented_redis'
 
-unless Rails.env.test?
+unless Rails.env.test? || Rails.env.assets?
   resque = CertManager::Configuration.resque
 
   Resque.redis = resque if resque
