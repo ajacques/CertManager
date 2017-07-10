3a73c269c5c88bad16d4848150d53edb0cd10148
Adam Jacques <adam@technowizardry.net>
2017-07-09 21:25:57 -0700

    Testing in prod

--- app/assets/javascripts/server_rendering.js
+++ app/assets/javascripts/server_rendering.js
@@ -1,2 +1,8 @@
-//= reuqire components
+//= require_self
+//= require react-server
+//= require common/common.js
+//= require js-routes
+//= require classnames
+//= require_tree ./controls/react
 //= require react_ujs
+var window = this;
