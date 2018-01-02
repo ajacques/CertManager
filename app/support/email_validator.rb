2e094e8af23a1228e5fbddd1f6543ca87be9e65b
Adam Jacques <adam@technowizardry.net>
2018-01-01 22:11:23 -0800

    More rubocop fixes

--- app/support/email_validator.rb
+++ app/support/email_validator.rb
@@ -1,7 +1,15 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
-      record.errors[attribute] << (options[:message] || 'is not a valid email address')
-    end
+    record.errors[attribute] << failure_message(options) unless passes_regex(value)
+  end
+
+  private
+
+  def failure_message(options)
+    options[:message] || 'is not a valid email address'
+  end
+
+  def passes_regex(value)
+    value =~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
   end
 end
