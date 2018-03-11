1e9d9f44ee749b47d6886bb078295f50f05b4d34
Adam Jacques <adam@technowizardry.net>
2018-03-11 11:34:05 -0700

    Throttle ACME sign attempt retries

--- app/jobs/acme_renew_job.rb
+++ app/jobs/acme_renew_job.rb
@@ -3,11 +3,21 @@ class AcmeRenewJob < ApplicationJob
 
   def perform(cert)
     @cert = cert
-    start_attempt unless cert.inflight_acme_sign_attempt
+    start_attempt if should_start_renewal(cert)
     AcmeImportJob.perform_later(cert.inflight_acme_sign_attempt)
   end
 
   private
+
+  def should_start_renewal(cert)
+    return false if cert.inflight_acme_sign_attempt
+
+    threshold = Time.now.utc - 6.hours
+    recent_attempts = cert.acme_sign_attempts.where('created_at > ? ', threshold).count
+
+    # Throttling - retry every 6 hours
+    recent_attempts.zero?
+  end
 
   def start_attempt
     settings = Settings::LetsEncrypt.new
